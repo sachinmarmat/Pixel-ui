0b699ee6fc4e060ce8600a41b2cf291cad49b9b3
sachinmarmat <sb6583425@gmail.com>
2025-09-10 10:05:59 +0530

    fg

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,28 +13,28 @@ const Login = () => {
                 style={{ backgroundImage: `url(${bglogin})` }}
             >
                 <div className=" container m-auto relative z-10 ">
-                    <div className=" flex pt-18 flex-col gap-7 px-5">
+                    <div className=" flex pt-4 sm:pt-18 flex-col gap-7 px-5">
                         {/* Logo + Brand */}
                         <div className="flex items-center gap-3">
                             <a href="/">
-                                <img src={img} alt="logo-img" className="w-16 rounded-4xl" />
+                                <img src={img} alt="logo-img" className="w-14 sm:w-16 rounded-4xl" />
                             </a>
-                            <h1 className="text-3xl font-medium text-white">Pixelgenix</h1>
+                            <h1 className="text-2xl md:text-3xl font-medium text-white">PixelGenix</h1>
                         </div>
 
                         {/* Login Box */}
-                        <div className="bg-gray-100/20  sm:p-7 rounded-3xl">
-                            <div className="flex flex-col lg:flex-row justify-center  items-center text-center">
+                        <div className="bg-gray-100/15  sm:p-7 rounded-3xl">
+                            <div className="flex flex-col lg:flex-row justify-center p-5 sm:p-3 md:p-0  items-center text-center">
                                 {/* Left Side */}
-                                <div className="flex flex-col items-center gap-10 lg:w-1/2">
-                                    <h1 className="text-3xl pt-2 sm:text-5xl  font-bold text-white">
-                                        Welcome <span className="text-red-500">Back!</span>
+                                <div className="flex flex-col items-center gap-8 sm:gap-10 lg:w-1/2">
+                                    <h1 className="text-3xl pt-2 sm:text-5xl  font-bold text-black md:text-white">
+                                        Welcome <span className="text-orange-600">Back!</span>
                                     </h1>
-                                    <img src={contectimg} alt="welcome" className="rounded-2xl max-w-xs sm:max-w-sm lg:max-w-md" />
+                                    <img src={contectimg} alt="welcome" className="rounded-2xl max-w-xs sm:max-w-sm pb-7  lg:max-w-md" />
                                 </div>
 
                                 {/* Right Side - Login Form */}
-                                <div className="flex flex-col gap-4  p-6 sm:p-8 rounded-2xl w-full lg:w-1/3 shadow-md">
+                                <div className="flex flex-col gap-4  p-7 px-5 md:px-8 sm:p-8 rounded-2xl w-full lg:w-1/3 shadow-md md:bg-gray-100/10 bg-gray-100/30 ">
                                     <h1 className="text-gray-900 text-2xl sm:text-3xl font-bold text-center">
                                         Login to Your Account
                                     </h1>
