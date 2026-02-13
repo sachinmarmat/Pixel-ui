955b35f4e9562564708bfea6184bf497e550f07c
sachinmarmat <sb6583425@gmail.com>
2026-02-13 13:08:54 +0530

    ow

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -141,7 +141,7 @@ const Contact = () => {
                             <div className="  w-full">
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.433839812462!2d75.75693107542894!3d26.88972321107823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db4f57f20e9d1%3A0xb0f74eb4950ed848!2sLaxman%20Colony%2C%20Shyam%20Nagar%2C%20Jaipur%2C%20Rajasthan%20302019!5e0!3m2!1sen!2sin!4v1757587874626!5m2!1sen!2sin"
-                                    className='w-90 h-75 pb-5 lg:pb-1 sm:w-180 lg:w-140 sm:h-70 rounded-xl sm:rounded'
+                                    className='w-94 h-75 pb-5 lg:pb-1 sm:w-180 lg:w-140 sm:h-70 rounded-xl sm:rounded'
                                     style={{ border: 0 }}
                                     allowFullScreen=""
                                     loading="lazy"
